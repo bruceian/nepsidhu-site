7df558813d1b1429c8c572eb607fba8f08a40ba6
Ian Bruce <brucegordonian@gmail.com>
2018-09-12 23:39:38 -0400

    ScrollMagic working well

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,51 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require turbolinks
+//= require jquery3
+//= require jquery_ujs
+//= require ScrollMagic.min
+//= require debug.addIndicators.min
+//= require TweenMax
+//= require animation.gsap.min
 //= require_tree .
+
+$(function() {
+
+  // var controller = new ScrollMagic.Controller({
+  //   globalSceneOptions: {
+  //     triggerHook: 'onLeave'
+  //   }
+  // });
+  //
+  // var slides = document.querySelectorAll("section.panel");
+  //
+  // for (var i=0; i<slides.length; i++ ) {
+  //   new ScrollMagic.Scene({
+  //     triggerElement: slides[i]
+  //   })
+  //   .setPin(slides[i])
+  //   .addIndicators()
+  //   .addTo(controller);
+  // }
+
+	// init
+	var controller = new ScrollMagic.Controller();
+
+	// define movement of panels
+	var wipeAnimation = new TimelineMax()
+    .fromTo("section.panel.blue", 1, {y: "0%"}, {y: "0%", ease: Linear.easeNone})  // in from left
+		.fromTo("section.panel.pink", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone})  // in from left
+		.fromTo("section.panel.purple", 1, {y:  "100%"}, {y: "0%", ease: Linear.easeNone})  // in from right
+		.fromTo("section.panel.green", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone}); // in from top
+
+	// create scene to pin and link animation
+	new ScrollMagic.Scene({
+			triggerElement: "#pinContainer",
+			triggerHook: "onLeave",
+			duration: "300%"
+		})
+		.setPin("#pinContainer")
+		.setTween(wipeAnimation)
+		.addIndicators() // add indicators (requires plugin)
+		.addTo(controller);
+});
