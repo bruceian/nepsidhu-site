f77e130c882c437a2df799cd88e1b374773e943b
Ian Bruce <brucegordonian@gmail.com>
2018-09-21 16:06:08 -0400

    swipe functioanlity improved for test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,20 +24,34 @@ $(function() {
 
 	// define movement of panels
 	var wipeAnimation = new TimelineMax()
-    .fromTo("section.panel.one", 1, {y: "0%"}, {y: "0%", ease: Linear.easeNone})  // in from left
-		.fromTo("section.panel.two", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone})  // in from left
-		.fromTo("section.panel.three", 1, {y:  "100%"}, {y: "0%", ease: Linear.easeNone})  // in from right
-		.fromTo("section.panel.four", 1, {y: "100%"}, {y: "-100%", ease: Linear.easeNone}); // in from top
+    .fromTo("section.panel.one", 1, {y: "0%"}, {y: "-100%", ease: Linear.easeNone})  // in from left
+		.fromTo("section.panel.two", 1, {y: "0%"}, {y: "-100%", ease: Linear.easeNone})  // in from left
+		.fromTo("section.panel.three", 1, {y:  "0%"}, {y: "-100%", ease: Linear.easeNone})  // in from right
 
 	// create scene to pin and link animation
 	new ScrollMagic.Scene({
       offset: 0,
 			triggerElement: "#pinContainer",
 			triggerHook: "onLeave",
-			duration: "1000%"
+			duration: "800%"
 		})
 		.setPin("#pinContainer")
 		.setTween(wipeAnimation)
 		// .addIndicators() // add indicators (requires plugin)
 		.addTo(controller);
+
+
+
+		var distance = $('section.three').offset().top;
+				$wrapper = $('#wrapper');
+			    $window = $(window);
+				console.log($window.scrollTop() - $window.height());
+
+			$window.scroll(function() {
+			    if ( ($window.scrollTop() - $window.height())  > distance ) {
+						$wrapper.css({
+							position: "relative"
+						});
+			    }
+			});
 });
