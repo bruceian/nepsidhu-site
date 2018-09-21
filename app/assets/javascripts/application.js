af446ed5c104e45e13aa43da188ac3e3c976e51d
Ian Bruce <brucegordonian@gmail.com>
2018-09-21 17:00:42 -0400

    removed scrolling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,43 +19,43 @@
 //= require_tree .
 
 $(function() {
-	// init
-	var controller = new ScrollMagic.Controller();
-
-	// define movement of panels
-	var wipeAnimation = new TimelineMax()
-    .fromTo("section.panel.one", 1, {y: "0%"}, {y: "-100%", ease: Linear.easeNone})  // in from left
-		.fromTo("section.panel.two", 1, {y: "0%"}, {y: "-100%", ease: Linear.easeNone})  // in from left
-		.fromTo("section.panel.three", 1, {y:  "0%"}, {y: "-100%", ease: Linear.easeNone})  // in from right
-
-	// create scene to pin and link animation
-	new ScrollMagic.Scene({
-      offset: 0,
-			triggerElement: "#pinContainer",
-			triggerHook: "onLeave",
-			duration: "500%"
-		})
-		.setPin("#pinContainer")
-		.setTween(wipeAnimation)
-		// .addIndicators() // add indicators (requires plugin)
-		.addTo(controller);
-
-
-
-		var distance = $('section.three').offset().top;
-		var bottom = $('section.three').position().top + $('section.three').outerHeight(true);
-				$wrapper = $('#wrapper');
-			    $window = $(window);
-					console.log(bottom);
-				console.log(distance);
-
-			$window.scroll(function() {
-				console.log(distance);
-			    if ( $window.height()  >= bottom ) {
-						$wrapper.css({
-							position: "relative",
-							top : "-100vh"
-						});
-			    }
-			});
+	// // init
+	// var controller = new ScrollMagic.Controller();
+	//
+	// // define movement of panels
+	// var wipeAnimation = new TimelineMax()
+  //   .fromTo("section.panel.one", 1, {y: "0%"}, {y: "-100%", ease: Linear.easeNone})  // in from left
+	// 	.fromTo("section.panel.two", 1, {y: "0%"}, {y: "-100%", ease: Linear.easeNone})  // in from left
+	// 	.fromTo("section.panel.three", 1, {y:  "0%"}, {y: "-100%", ease: Linear.easeNone})  // in from right
+	//
+	// // create scene to pin and link animation
+	// new ScrollMagic.Scene({
+  //     offset: 0,
+	// 		triggerElement: "#pinContainer",
+	// 		triggerHook: "onLeave",
+	// 		duration: "500%"
+	// 	})
+	// 	.setPin("#pinContainer")
+	// 	.setTween(wipeAnimation)
+	// 	// .addIndicators() // add indicators (requires plugin)
+	// 	.addTo(controller);
+	//
+	//
+	//
+	// 	var distance = $('section.three').offset().top;
+	// 	var bottom = $('section.three').position().top + $('section.three').outerHeight(true);
+	// 			$wrapper = $('#wrapper');
+	// 		    $window = $(window);
+	// 				console.log(bottom);
+	// 			console.log(distance);
+	//
+	// 		$window.scroll(function() {
+	// 			console.log(distance);
+	// 		    if ( $window.height()  >= bottom ) {
+	// 					$wrapper.css({
+	// 						position: "relative",
+	// 						// top : "-100vh"
+	// 					});
+	// 		    }
+	// 		});
 });
